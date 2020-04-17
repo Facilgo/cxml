23d66f30f6921da880d4a6c8c09fd704de4278a0
rorong <rohitporwal4888@gmail.com>
2020-04-17 18:21:35 +0530

    Allow To Send ItemOut Attributes IN PunchoutSetupRequest

--- lib/cxml/item_out.rb
+++ lib/cxml/item_out.rb
@@ -8,14 +8,18 @@ module CXML
 
         @item_id = data['ItemID']
         @supplier_part_id = @item_id['SupplierPartID']
+        @supplier_part_auxiliary_id = @item_id['SupplierPartAuxiliaryID']
 
         @item_detail = data['ItemDetail']
-        @unit_price = @item_detail['UnitPrice']
-        @classification = CXML::Classification.new(@item_detail['Classification']) if @item_detail['Classification']
-        @description = CXML::Description.new(@item_detail['Description']) if @item_detail['Description']
-        @unit_of_measure = @item_detail['UnitOfMeasure']
 
-        @shipping = data['Shipping'] if data['Shipping']
+        if @item_detail
+          @unit_price = @item_detail['UnitPrice']
+          @classification = CXML::Classification.new(@item_detail['Classification']) if @item_detail['Classification']
+          @description = CXML::Description.new(@item_detail['Description']) if @item_detail['Description']
+          @unit_of_measure = @item_detail['UnitOfMeasure']
+        end
+
+        @shipping = data['Shipping']
         @extrinsic = data['Extrinsic']
       end
     end
@@ -25,13 +29,18 @@ module CXML
         t.ItemID do |o|
           o.SupplierPartID { |p| p.text(@supplier_part_id['content']) }
         end
-        t.ItemDetail do |o|
-          o.UnitPrice { |p| CXML::Money.new(@unit_price['Money']).render(p) }
-          @description.render(o) if @description
-          o.UnitOfMeasure do |p|
-            p.text @unit_of_measure['content']
+        t.ItemID do |o|
+          o.SupplierPartAuxiliaryID { |p| p.text(@supplier_part_auxiliary_id['content']) }
+        end
+        if @item_detail
+          t.ItemDetail do |o|
+            o.UnitPrice { |p| CXML::Money.new(@unit_price['Money']).render(p) }
+            @description.render(o) if @description
+            o.UnitOfMeasure do |p|
+              p.text @unit_of_measure['content']
+            end
+            @classification.render(o) if @classification
           end
-          @classification.render(o) if @classification
         end
         if @shipping
           t.Shipping do |o|
--- lib/cxml/punch_out_steup_request.rb
+++ lib/cxml/punch_out_steup_request.rb
@@ -7,6 +7,12 @@ module CXML
         @buyer_cookie = CXML::BuyerCookie.new(data['BuyerCookie'])
         @extrinsic = data['Extrinsic']
         @browser_form_post = data['BrowserFormPost']
+        @item_outs = []
+        if data['ItemOut'] && data['ItemOut'].is_a?(Array)
+          data['ItemOut'].each do |item|
+            @item_outs << CXML::ItemOut.new(item)
+          end
+        end
         @ship_to = data['ShipTo']
       end
     end
@@ -21,6 +27,9 @@ module CXML
         end
         n.BrowserFormPost do |t|
           CXML::Url.new(@browser_form_post['URL']).render(t)
+        end
+        @item_outs.each do |item_out|
+          item_out.render(n)
         end
         n.ShipTo    { |t| CXML::Address.new(@ship_to['Address']).render(t) }
       end
